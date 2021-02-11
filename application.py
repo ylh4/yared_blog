e297e90565ec52f21b96bdcef82fb470bdd1246c
yared.hurisa <yared.hurisa@kimetrica.com>
2021-02-11 05:29:49 -0500

    update3

--- application.py
+++ application.py
@@ -1,8 +1,5 @@
-from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
-
+from flask import Flask, render_template, url_for
 application = Flask(__name__)
-application.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 posts = [
     {
@@ -29,27 +26,6 @@ def home():
 @application.route("/about")
 def about():
     return render_template('about.html', title='About')
-
-
-@application.route("/register", methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
-    return render_template('register.html', title='Register', form=form)
-
-
-@application.route("/login", methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
-        else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
