787999dbcb56463d3a42571e56e581ddf418b949
yared.hurisa <yared.hurisa@kimetrica.com>
2021-02-12 21:17:20 -0500

    back to init

--- application/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, validators
-
-class EnterDBInfo(FlaskForm):
-    dbNotes = StringField(label='Items to add to DB', description="db_enter", validators=[validators.required(), validators.Length(min=0, max=128, message=u'Enter 128 characters or less')])    
-
-class RetrieveDBInfo(FlaskForm):
-    numRetrieve = StringField(label='Number of DB Items to Get', description="db_get", validators=[validators.required(), validators.Regexp('^\d{1}$',message=u'Enter a number between 1 and 10')])
